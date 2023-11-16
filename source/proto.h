7303ce8722b5a50cbd4ad057897fc51d1c343ca7
osan <osannnn@gmail.com>
2023-11-16 16:41:34 +0100

    created prototypes in proto.h

--- source/proto.h
+++ source/proto.h
@@ -2,15 +2,15 @@
 #define PROTO_H
 
 
-//richiesta di tipo di formato della data e output del valore del formato
-
+//richiesta di tipo di formato della data e output del valore del formato (1-3)
+int dateFormat();
 //input utente della data e controllo della validità di quest'ultima
-
+void dateGetValidate(char input[], int dimensione[], int formato);
 //conversione da char a int
-
+void dateConversion(char dataIn[], int dataOut[]);
 //elaborazione dei dati e output dei risultati
-
+void daysPassed(int data1[], int data2[]);
 //menù
-
+int menu();
 
 #endif
