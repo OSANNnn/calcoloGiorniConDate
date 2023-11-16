23746bdc98ca441b37e8e80575cb7d666600ff4f
osan <osannnn@gmail.com>
2023-11-16 16:17:13 +0100

    declared variables in main and done the skeleton of the program

---
+++ source/proto.h
@@ -0,0 +1,16 @@
+#ifndef PROTO_H
+#define PROTO_H
+
+
+//richiesta di tipo di formato della data e output del valore del formato
+
+//input utente della data e controllo della validità di quest'ultima
+
+//conversione da char a int
+
+//elaborazione dei dati e output dei risultati
+
+//menù
+
+
+#endif
