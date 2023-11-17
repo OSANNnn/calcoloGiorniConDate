e910aa0beb6c8871a40332f3d0b009e093ad7a33
osan <osannnn@gmail.com>
2023-11-17 12:54:40 +0100

    removed variable posizione

--- source/input.c
+++ source/input.c
@@ -39,7 +39,30 @@ void dateGetValidate(char input[], int dimensione, int formato){
         valida    = 1;
         contaChar = 0;
 
-        if (formato == 1){
+        if (formato == 1){ //gg/mm/aaaa
+            for (int i = 0; i < 10 && valida; i++){
+
+                if ((appoggio[i] != '/' && (appoggio[i] >= '0' && appoggio[i] <= '9')) && contaChar <= 1 && i < 2){
+                    input[i + 6] = appoggio[i];
+                    contaChar++;
+                }        
+                else if ((appoggio[i] != '/' && (appoggio[i] >= '0' && appoggio[i] <= '9')) && (contaChar >= 1 && contaChar <= 3) && i > 2){
+                    input[i + 1] = appoggio[i];
+                    contaChar++;
+                }               
+                else if ((appoggio[i] != '/' && (appoggio[i] >= '0' && appoggio[i] <= '9')) && (contaChar >= 3 && contaChar <=7)  && i > 5){
+                    input[i - 6] = appoggio[i];
+                    contaChar++;
+                }
+                else if (appoggio[i] != '/') {
+                    printf("Formato non valido.\n");
+                    valida = 0;
+                }
+            }
+            input[8] = '\0';
+            printf("%s \n",input);
+        }/*
+        else if (formato == 2){
             posizione = 0;
 
             for (int i = 0; i < 10 && valida; i++){
@@ -66,6 +89,6 @@ void dateGetValidate(char input[], int dimensione, int formato){
             }
             input[8] = '\0';
             printf("%s \n",input);
-        }
+        }*/
     }while (!valida);
 }
