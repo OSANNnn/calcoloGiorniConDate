0517f1eb3352a393e56e63801840fb2301e0c4bd
osan <osannnn@gmail.com>
2023-11-21 12:45:55 +0100

    fixed stuff

--- source/input.c
+++ source/input.c
@@ -24,7 +24,6 @@ int dateFormat(){
 void dateGetValidate(char input[], int dimensione, int formato){
     char appoggio[dimensione];
     char stringaFormato[4][11];
-    int posizione;
     int contaChar;
     int valida;
 
@@ -34,61 +33,76 @@ void dateGetValidate(char input[], int dimensione, int formato){
 
     do{
         printf("Inserisci la data %s: ", stringaFormato[formato]);
-        scanf("%s", &appoggio);
+        scanf("%s", input);
         while(getchar() != '\n');
         valida    = 1;
-        contaChar = 0;
-
-        if (formato == 1){ //gg/mm/aaaa
-            for (int i = 0; i < 10 && valida; i++){
-
-                if ((appoggio[i] != '/' && (appoggio[i] >= '0' && appoggio[i] <= '9')) && contaChar <= 1 && i < 2){
-                    input[i + 6] = appoggio[i];
+        contaChar = 0; //conta i caratteri diversi da '/'           |a|b|/|c|d|/|e|f|g|h|
+                                                        //          |0|1|2|3|4|5|6|7|8|9|
+        if (formato == 1){ //gg/mm/aaaa                             |e|f|g|h|c|d|a|b|-|-|
+            for (int i = 0; i < dimensione-1 && valida; i++){
+                if ((input[i] != '/' && (input[i] >= '0' && input[i] <= '9')) && contaChar < 2 && i < 2){
+                    appoggio[i + 6] = input[i];
                     contaChar++;
                 }        
-                else if ((appoggio[i] != '/' && (appoggio[i] >= '0' && appoggio[i] <= '9')) && (contaChar >= 1 && contaChar <= 3) && i > 2){
-                    input[i + 1] = appoggio[i];
+                else if ((input[i] != '/' && (input[i] >= '0' && input[i] <= '9')) && (contaChar >= 2 && contaChar <= 3) && i >= 2){
+                    appoggio[i + 1] = input[i];
                     contaChar++;
                 }               
-                else if ((appoggio[i] != '/' && (appoggio[i] >= '0' && appoggio[i] <= '9')) && (contaChar >= 3 && contaChar <=7)  && i > 5){
-                    input[i - 6] = appoggio[i];
+                else if ((input[i] != '/' && (input[i] >= '0' && input[i] <= '9')) && (contaChar >= 3 && contaChar <=7)  && i >= 5){
+                    appoggio[i - 6] = input[i];
                     contaChar++;
                 }
-                else if (appoggio[i] != '/') {
+                else if (input[i] != '/') {
                     printf("Formato non valido.\n");
                     valida = 0;
                 }
             }
-            input[8] = '\0';
-            printf("%s \n",input);
-        }/*
-        else if (formato == 2){
-            posizione = 0;
-
-            for (int i = 0; i < 10 && valida; i++){
-
-                if ((appoggio[i] != '/' && (appoggio[i] >= '0' && appoggio[i] <= '9')) && contaChar <= 1 && i < 2){
-                    input[posizione + 6] = appoggio[i];
-                    posizione++;
+            appoggio[8] = '\0';
+            printf("%s \n", appoggio); //debug
+        }
+        else if (formato == 2){ //mm/gg/aaaa
+            for (int i = 0; i < dimensione-1 && valida; i++){
+                if ((input[i] != '/' && (input[i] >= '0' && input[i] <= '9')) && contaChar < 2 && i < 2){
+                    appoggio[i + 4] = input[i];
                     contaChar++;
                 }        
-                else if ((appoggio[i] != '/' && (appoggio[i] >= '0' && appoggio[i] <= '9')) && (contaChar >= 1 && contaChar <= 3) && i > 2){
-                    input[posizione + 2] = appoggio[i];
-                    posizione++;
+                else if ((input[i] != '/' && (input[i] >= '0' && input[i] <= '9')) && (contaChar >= 2 && contaChar <= 3) && i >= 2){
+                    appoggio[i + 3] = input[i];
                     contaChar++;
                 }               
-                else if ((appoggio[i] != '/' && (appoggio[i] >= '0' && appoggio[i] <= '9')) && (contaChar >= 3 && contaChar <=7)  && i > 5){
-                    input[posizione - 4] = appoggio[i];
-                    posizione++;
+                else if ((input[i] != '/' && (input[i] >= '0' && input[i] <= '9')) && (contaChar >= 3 && contaChar <=7)  && i >= 5){
+                    appoggio[i - 6] = input[i];
                     contaChar++;
                 }
-                else if (appoggio[i] != '/') {
+                else if (input[i] != '/') {
                     printf("Formato non valido.\n");
                     valida = 0;
                 }
             }
-            input[8] = '\0';
-            printf("%s \n",input);
-        }*/
+            appoggio[8] = '\0';
+            printf("%s \n", appoggio); //debug
+        }
+        else if (formato == 3){ //aaaa/mm/gg
+            for (int i = 0; i < dimensione-1 && valida; i++){
+                if ((input[i] != '/' && (input[i] >= '0' && input[i] <= '9')) && contaChar < 4 && i < 4){
+                    appoggio[i] = input[i];
+                    contaChar++;
+                }        
+                else if ((input[i] != '/' && (input[i] >= '0' && input[i] <= '9')) && (contaChar >= 3 && contaChar <= 5) && i > 4){
+                    appoggio[i - 1] = input[i];
+                    contaChar++;
+                }               
+                else if ((input[i] != '/' && (input[i] >= '0' && input[i] <= '9')) && (contaChar >= 6 && contaChar <=7)  && i > 7){
+                    appoggio[i - 2] = input[i];
+                    contaChar++;
+                }
+                else if (input[i] != '/') {
+                    printf("Formato non valido.\n");
+                    valida = 0;
+                }
+            }
+            appoggio[8] = '\0';
+            printf("%s \n", appoggio); //debug
+        }
     }while (!valida);
 }
--- source/main.c
+++ source/main.c
@@ -9,18 +9,19 @@
 #include <stdio.h>
 
 int main(){
-    const int DIMENSIONE_INPUT        = 9; // mm + gg + aaaa + 2*/ basato sul tipo di input + /0
+    const int DIMENSIONE_INPUT        = 11; // mm + gg + aaaa + 2*/ basato sul tipo di input + /0
     const int DIMENSIONE_ELABORAZIONE = 3;  //0 = giorno, 1 = mese, 2 = anno
     const int DIMENSIONE_MESI         = 13; // 0 = 365, 1 = Gennaio = 31, 2 = febbraio = 28, ... , 12 = dicembre = 31
     
-    char dataInput[DIMENSIONE_INPUT]; 
+    char dataInput1[DIMENSIONE_INPUT], dataInput2[DIMENSIONE_INPUT];
     int data1[DIMENSIONE_ELABORAZIONE], data2[DIMENSIONE_ELABORAZIONE];
     int mesi[] = {361, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     char formato; //formato data 1-3
     char scelta;  //ripeti programma
 
-    dateGetValidate(dataInput, DIMENSIONE_INPUT, dateFormat());
+    formato = dateFormat();
+    dateGetValidate(dataInput1, DIMENSIONE_INPUT, formato);
 
     /*do{
         //menu
