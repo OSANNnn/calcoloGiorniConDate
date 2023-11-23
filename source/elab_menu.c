ad77ea16371e07377f99df1c7e2a08f2eb6b584e
osan <osannnn@gmail.com>
2023-11-23 16:36:46 +0100

    added file with definition of menu and elab

---
+++ source/elab_menu.c
@@ -0,0 +1,30 @@
+#include "proto.h"
+#include <stdio.h>
+
+
+int menu(){
+    char scelta;
+
+    do{
+        printf("Continuare?\n");
+        printf("[1] Continua. \n[0] Esci\n");
+        scanf(" %c", &scelta);
+        while(getchar() != '\n');
+
+        if (scelta < '0' || scelta > '1'){
+            printf("Comando non valido.\n\n");
+        }
+        
+    }while(scelta < '0' || scelta > '1');
+
+    return scelta - 48;
+}
+
+
+void dateInversion(char dataChar1[], char dataChar2, int dimensione, int dataInt1, int dataInt2){
+}
+
+
+void dateElaboration(int data1[], int data2[], const int arrayMesi) {
+
+}
